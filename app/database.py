0b27718d6cf5f9fa5089bbc71e3cca0171ba34a9
Vicrrs <enricofermi2019@gmail.com>
2024-02-16 04:03:34 -0400

    database

--- app/database.py
+++ app/database.py
@@ -4,7 +4,11 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "postgresql://password@db:5432/todo_db"
 
+# create_engine: Cria a conexão com o banco de dados PostgreSQL
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# SessionLocal: é uma fabrica de sessões que permite transações com o banco
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Base: é uma classe para todos os modelos declarativos
 Base = declarative_base()
